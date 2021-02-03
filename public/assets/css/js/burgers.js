eb981fb396dd64c7c79105daff846623b001bdbb
Abuye Mamuye <abuyeye3@gmail.com>
2021-02-02 19:10:28 -0800

    create the main handlebars

--- public/assets/css/js/burgers.js
+++ public/assets/css/js/burgers.js
@@ -7,21 +7,21 @@ $(function() {
         devour: newDevour
       };
   
-      // Send the PUT request.
+      
       $.ajax("/api/burgers/" + id, {
         type: "PUT",
         data: newDevourState
       }).then(
         function() {
           console.log("changed devour to", newDevour);
-          // Reload the page to get the updated list
+          
           location.reload();
         }
       );
     });
   
     $(".create-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
+     
       event.preventDefault();
   
       var newBurger = {
@@ -29,31 +29,31 @@ $(function() {
         devour: $("[name=devour]:checked").val().trim()
       };
   
-      // Send the POST request.
+     
       $.ajax("/api/burgers", {
         type: "POST",
         data: newBurger
       }).then(
         function() {
           console.log("created new burger");
-          // Reload the page to get the updated list
+          
           location.reload();
         }
       );
     });
   
-    // $(".delete-cat").on("click", function(event) {
-    //   var id = $(this).data("id");
+    $(".delete-cat").on("click", function(event) {
+      var id = $(this).data("id");
   
-    //   // Send the DELETE request.
-    //   $.ajax("/api/cats/" + id, {
-    //     type: "DELETE"
-    //   }).then(
-    //     function() {
-    //       console.log("deleted cat", id);
-    //       // Reload the page to get the updated list
-    //       location.reload();
-    //     }
-    //   );
-    // });
+    
+      $.ajax("/api/cats/" + id, {
+        type: "DELETE"
+      }).then(
+        function() {
+          console.log("deleted cat", id);
+          
+          location.reload();
+        }
+      );
+    });
   });
